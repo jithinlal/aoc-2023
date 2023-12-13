705de1ebb3209064317bcb91df4dd3107ea56b35
jithinlal-gelato <jithinlal.pr@gelato.com>
2023-12-13 19:23:22 +0530

    day 13 - not my solution

---
+++ 13.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('13.txt', 'utf8');
+
+const parseInput = (rawInput) => {
+    return rawInput.split("\n\n").map((e) => e.split("\n"));
+};
+
+function solution(rows, diffPossible) {
+
+    let sum = 0;
+
+    for (const row of rows) {
+        const lenRow = row.length;
+        const lenCol = row[0].length;
+
+        let rowsBefore = 0;
+        for (let i = 0; i < lenRow - 1; i++) {
+            let diffCount = 0;
+            for (let x = Math.min(i * 2 + 1, lenRow - 1); x > i; x--) {
+                for (let j = 0; j < lenCol; j++) {
+                    if (row[2 * i + 1 - x][j] !== row[x][j]) {
+                        diffCount++;
+                    }
+                    if (diffCount > diffPossible) {
+                        break;
+                    }
+                }
+            }
+
+            if (diffCount === diffPossible) {
+                rowsBefore = i + 1;
+            }
+        }
+        let columnsBefore = 0;
+
+        for (let i = 0; i < lenCol - 1; i++) {
+            let diffCount = 0;
+            for (let x = Math.min(i * 2 + 1, lenCol - 1); x > i; x--) {
+                for (let j = 0; j < lenRow; j++) {
+                    if (row[j][2 * i + 1 - x] !== row[j][x]) {
+                        diffCount++;
+                    }
+                    if (diffCount > diffPossible) {
+                        break;
+                    }
+                }
+            }
+
+            if (diffCount === diffPossible) {
+                columnsBefore = i + 1;
+            }
+        }
+
+        sum = sum + columnsBefore + 100 * rowsBefore;
+    }
+    return sum
+}
+
+const part1 = (rawInput) => {
+    const diffPossible = 0;
+    const rows = parseInput(rawInput);
+
+    return solution(rows, diffPossible)
+};
+
+const part2 = (rawInput) => {
+    const diffPossible = 1;
+    const rows = parseInput(rawInput);
+
+    return solution(rows, diffPossible)
+};
+
+
+console.log(part2(input))
